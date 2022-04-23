b80a63abc89e3ccde23f2223a58b8f39eec441dc
Joaodcs <jd.cabrals@gmail.com>
2022-04-23 09:22:20 -0300

    Color Flipper v1.0

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,17 @@
+const colors = ["#0396FF", "#EA5455", "#7367F0", "#8C1BAB", "#F55555", "#9F44D3", "#3677FF", "#FA016D", "#002661", "#0E5CAD"];
+
 document.getElementById("btn").addEventListener("click",changeColor);
 
 function changeColor(button)
 {
     let bg = document.getElementById("container");
     let idcolor = document.getElementById("id-color");
-    let index = 1;
+    const randomNumber = getRandomNumber();
+    bg.style.backgroundColor = colors[randomNumber];
+    idcolor.textContent = colors[randomNumber];
+}
 
-    switch(index)
-    {
-        case 1:
-            bg.style.backgroundColor = "#0396FF";
-            idcolor.textContent = "#0396FF";
-            index = index+1;
-            break;
-        case 2:
-            bg.style.backgroundColor = "#623AA2";
-            idcolor.textContent = "#623AA2";
-            index = index+1;
-            break;
-        default:
-            bg.style.backgroundColor = "#000000";
-            idcolor.textContent = "#000000";
-            index = "#000000";
-    }
+function getRandomNumber()
+{
+    return Math.floor(Math.random() * 10);
 }
